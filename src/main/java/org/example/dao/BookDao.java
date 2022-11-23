61f5404040bd7357f4751102bacdd672093e92b3
TrueTony <spanchbob@gmail.com>
2022-11-23 17:25:02 +0400

    fix imports

--- src/main/java/org/example/dao/BookDao.java
+++ src/main/java/org/example/dao/BookDao.java
@@ -1,14 +1,8 @@
 package org.example.dao;
 
-import org.example.models.Book;
-import org.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class BookDao {
